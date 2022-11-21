efe10deaf7d62543944929d9bf11690a2757821a
akii1998 <110331686+akii1998@users.noreply.github.com>
2022-11-22 00:59:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-  
+  const [text, setText] = useState(true);
+  const handleChange = () => {
+    setText(!text);
+  };
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{text ? "Macro" : "Polo"}</h1>
+      <button onClick={handleChange}>{text ? "Polo" : "Macro"}</button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
